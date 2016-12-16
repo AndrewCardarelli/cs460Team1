3e7dc2afc446480200f87f29477eebf39c277a8c
AndrewCardarelli <acards2400@gmail.com>
2016-12-16 12:14:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
 <?php
-
- $dbhost = "us-cdbr-azure-east-c.cloudapp.net";
- $dbuser = "bb4ff2b5583709";
- $dbpass = "63ce33fa";
- $conn = mysql_connect($dbhost, $dbuser, $dbpass);
- $dbname = 'team1webapp';
-
- echo "<p>Select a Department</p>";
- mysql_select_db($dbname, $conn) or die ("Error selecting specified database on mysql server: ".mysql_error());
+ echo '<body background ="Plain Background1.jpg">';
+ echo '<br> <br> <br> <br>';
+ include 'connectDatabase.php';
+ connectDatabase();
+ echo "<p><b><font face= 'arial black' size='4'>Select a Department</b></font></p>";
+ //mysql_select_db($dbname, $conn) or die ("Error selecting specified database on mysql server: ".mysql_error());
  $mmquery= 'SELECT dept_name FROM dept_table';
  // $mmquery= 'SELECT dept_name FROM dept_table';
  $mmresult= mysql_query($mmquery) or die ("Query to get data from firsttable failed: ".mysql_error());
  //echo '<form method = "post" action= "StartPage.php" >';
  echo '<form method = "post" action= "index.php" >';
- echo "<select name = $dept>";
+ echo "<select name = dept>";
  while ($mmrow=mysql_fetch_array($mmresult)){
      $mmTitle = $mmrow["dept_name"];
      echo "<option>
         $mmTitle
     </option>";
  }
+ echo "</select>";
  
  echo '<form method = "post" action= "Script2">';
- echo "<input type='submit' value = 'submit the form'/>";
+ echo "<input type='submit' value = 'find contact'/>";
  echo "</form>";
+getContact();
  
- echo "<p> <b>Majors</b> </p>";
+ echo "<p><b><font face= 'arial' size='4' color = 'white'>Majors</b></font></p>";
  echo '<form method = "post" action= "CISMajor.php">';
  echo "<input type='submit' value = 'CIS Major'/>";
  echo "</form>";
@@ -42,7 +41,7 @@
  echo "<input type='submit' value = 'Actuarial Science Major'/>";
  echo "</form>";
 
- echo "<p> <b>Minors</b> </p>";
+ echo "<p><b><font face= 'arial' size='4' color = 'white'>Minors</b></font></p>";
  echo '<form method = "post" action= "PoliticsMinor.php">';
  echo "<input type='submit' value = 'Politics Minor'/>";
  echo "</form>";
@@ -63,9 +62,49 @@
  echo "<input type='submit' value = 'Psychology Minor'/>";
  echo "</form>";
 
+  echo "<p><b><font face= 'arial black' size='4'>Search by course number</b></font></p>";
+ echo '<form action="show.php" method="post">' ;
+ echo '<input type="text" value="" name="a" class="textfield_effect" maxlength="10" onfocus="this.value='; 
+ echo "'";
+ echo '">';
+ echo '<input type="submit" value="Submit" />'; 
+ echo '</form>';
+
  $submittedValue = '';
  if (isset($_POST["depart_name"])) {
      $submittedValue = $_POST["depart_name"];
  }
- 
+ function getContact(){
+    $sqlform = "SELECT * FROM dept_table WHERE dept_name LIKE '". $_POST['dept']."'";
+    $result_form =mysql_query($sqlform);
+    while ($allrow=mysql_fetch_array($result_form)){
+        $dept_name = $allrow["dept_name"];
+        $dept_chair = $allrow["dept_chair"];
+        $office = $allrow["dept_office"];
+        $email = $allrow["dept_email"];
+        $phone = $allrow["dept_phone"];
+
+  echo "<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}";
+        echo "</style>";
+        echo "<table>
+            <tr>
+                <td BGCOLOR='#ffffff'><b>Department</b><br />$dept_name</td>
+                <td BGCOLOR='#ffffff'><b>Department Chair</b><br />$dept_chair</td>
+                <td BGCOLOR='#ffffff'><b>Office Location</b><br />$office</td>
+                <td BGCOLOR='#ffffff'><b>Email</b><br />$email</td>
+                <td BGCOLOR='#ffffff'><b>Phone Number</b><br />$phone</td>
+       
+            </tr>";
+        echo "</table>";
+
+    }
+}
+echo '</body>';
  ?>
