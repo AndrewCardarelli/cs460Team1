2679439d48559380e59fb7d6f388b4157f388d26
AndrewCardarelli <acards2400@gmail.com>
2016-12-18 15:57:50 -0500

    Update CISMajor.php

--- CISMajor.php
+++ CISMajor.php
@@ -1,5 +1,6 @@
 <?php
-echo '<body background ="Course Information1.jpg">';
+echo '<link href="StyleSheet2.css" rel="stylesheet">';
+//echo '<body background ="Course Information1.jpg">';
 include 'connectDatabase.php';
 connectDatabase();
 echo "<p><font face= 'verdana'>Select a Course</font></p>";
